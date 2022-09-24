d6fa1b2d32b4be8d7a1c7f6eb448d27283ad3ea2
Muhammad Zain <112485536+muhammadzain001@users.noreply.github.com>
2022-09-24 12:27:57 +0300

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,18 @@
+# Lists
+
+var1 = [5, 4, 3, 2, 1]
+print(5 in var1)
+print(10 in var1)
+var2 = [2.619, 4.203]
+var3 = ["blue", "green", "red", "yellow", "purple", "orange"]
+var4 = [True, False, False, True, False, True, True, False, True]
+var5 = [[1, 2, 3], [4,5], [6, 7, 8, 9]]
+var6 = [10, 3.14, "tree", False, [1, 2, 3]]
+
+print(list("Hellow"))
+
+
+var6 = [10, 3.14, "tree", False, [1, 2, 3]]
+var_list = list("cheese")
+print("e" in var_list)
+print("a" not in var_list)
